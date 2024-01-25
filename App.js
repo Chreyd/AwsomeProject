7e0d9470779ed38a649d299f15f86a7522f5ab54
Fleudry <chreydmapeba@gmail.com>
2024-01-25 02:12:33 +0100

    Composant Button

--- App.js
+++ App.js
@@ -1,27 +1,16 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
 import React from "react";
-
-const Person = (props) => {
-  return (
-    <View>
-      {props.age ? (
-        <Text style={styles.text}>
-        -  Je suis: {props.name} , j'ai {props.age} ans et je suis un {props.children}
-        </Text>
-      ) : (
-        <Text style={styles.text}>-  Je suis: .{props.name} et je suis un {props.children}</Text>
-      )}
-    </View>
-  );
-};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}></Text>
-      <Person name="Roger" age="43" >Animal</Person>
-      <Person name="Stan" >Humain</Person>
-      <Person name="Steve" age="58" >Humain</Person>
+      <Button
+        // onPress={onPressLearnMore}
+        title="Cliquez ICI"
+        color="red"
+        accessibilityLabel="Learn more about this purple button"
+        disabled={false}
+      />
     </View>
   );
 }
